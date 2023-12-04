fa4f3f07171024cfa270ce7c59afdb8b23ded6e2
23JW4346 <23JW4346@barton.ac.uk>
2023-12-04 11:35:39 +0000

    changes

---
+++ December 3rd part 1 again/December 3rd part 2.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+class Program
+{
+    static void listPrint(List<int> nums)
+    {
+        Console.Write("[");
+        foreach (int i in nums)
+        {
+            Console.Write(i + ", ");
+        }
+        Console.WriteLine("]");
+    }
+    static List<int> GetAdjacentNumbers(string[] lines, int row, int col)
+    {
+        List<int> numbers = new List<int>();
+        int sum = 0;
+        for (int i = row - 1; i <= row + 1; i++)
+        {
+            for (int j = col - 1; j <= col + 1; j++)
+            {
+                if (i >= 0 && i < lines.Length && j >= 0 && j < lines[i].Length &&
+                    (i != 0 || j!= 0) && char.IsNumber(lines[i][j]))
+                {
+                    sum += lines[i][j];
+                    int k = 1;
+                    while (Char.IsNumber(lines[i][j]))
+                    {
+                        sum += lines[i][j + k];
+                        k++;
+                    }
+                    k = 1;
+                    while (Char.IsNumber(lines[i][j]))
+                    {
+                        sum = lines[i][j - k] + sum;
+                        k++;
+                    }
+                    numbers.Add(int.Parse(sum.ToString()));
+                }
+            }
+        }
+        listPrint(numbers);
+        return numbers;
+    }
+    static int CalculateGearRatios(string[] engine)
+    {
+        List<int> gearRatios = new List<int>();
+        for (int row = 0; row < engine.Length; row++)
+        {
+            for (int col = 0; col < engine[row].Length; col++)
+            {
+                if (engine[row][col] == '*')
+                {
+                    List<int> adjacentNumbers = GetAdjacentNumbers(engine, row, col);
+                    if (adjacentNumbers.Count == 2)
+                    {
+                        int gearRatio = 1;
+                        for (int x = 0; x < adjacentNumbers.Count; x++)
+                        {
+                            gearRatio *= adjacentNumbers[x];
+                        }
+                        gearRatios.Add(gearRatio);
+                        Console.WriteLine(gearRatio);
+                    }
+                    
+                }
+            }
+        }
+        return gearRatios.Sum();
+    }
+    static void Main(string[] args)
+    {
+        string[] engineSchematic = File.ReadAllLines("input 3.txt");
+        int result = CalculateGearRatios(engineSchematic);
+        Console.WriteLine(result);
+        Console.ReadKey();
+    } 
+}
+
---
+++ December 4th part 1/December 4th part 1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace December_4th_part_1
+{
+    internal class Program
+    {
+        static int compare(List<string> winning, List<string> your)
+        {
+            int points = 0; 
+            for(int i = 0; i < your.Count; i++)
+            {
+                if (winning.Contains(your[i])) points++;
+            }
+            if (points == 0) return 0;
+            else if (points > 0) return (int)Math.Pow(2, points - 1);
+            else return 0;
+        }
+        static void string_spliter(string line, ref int result)
+        {
+            string[] split = line.Split(':')[1].Trim().Split('|');
+            List<string> winnum = new List<string>(split[0].Trim().Split(' '));
+            while (winnum.Contains("")) winnum.Remove("");
+            List<string> yournum = new List<string>(split[1].Trim().Split(' '));
+            while (yournum.Contains("")) yournum.Remove("");
+            result += compare(winnum, yournum);
+        }
+        static void Main(string[] args)
+        {
+            int result=0;
+            string[] lines = File.ReadAllLines("input 4.txt");
+            foreach(string line in lines)
+            {
+                string_spliter(line, ref result);
+            }
+            Console.WriteLine($"Final Result: {result}");
+            Console.ReadKey();
+        }
+    }
+}
